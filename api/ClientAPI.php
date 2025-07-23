30f376d9acd09c59b3226116046e1fa0a1b19e63
ThomasdevLC <thomas.lecam@gmail.com>
2025-07-23 09:58:34 +0200

    Initial commit: auth + API client

---
+++ api/ClientAPI.php
@@ -0,0 +1,75 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+
+function callApi($method, $endpoint, $data = null, $customHeaders = null): array
+{
+    $url = API_BASE_URL . $endpoint;
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
+
+    $headers = $customHeaders ?? API_HEADERS;
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    if ($data !== null) {
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    }
+
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if (curl_errno($ch)) {
+        return ['code' => 500, 'message' => curl_error($ch)];
+    }
+
+    curl_close($ch);
+
+    return [
+        'http_code' => $httpCode,
+        'body' => json_decode($response, true)
+    ];
+}
+
+
+function getAuthToken()
+{
+    $url = API_BASE_URL . 'auth';
+
+    $data = [
+        'username' => 'test_api',
+        'password' => 'api123456',
+        'password_type' => 0,
+        'code_application' => 'webservice_externe',
+        'code_version' => '1'
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: application/api.rest-v1+json',
+        'Content-Type: application/json'
+    ]);
+
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if (curl_errno($ch)) {
+        return null;
+    }
+
+    curl_close($ch);
+
+    $result = json_decode($response, true);
+
+    if ($httpCode === 200 && isset($result['datas']['token'])) {
+        return $result['datas']['token'];
+    }
+
+    return null;
+}
+
+
+
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('API_BASE_URL', 'https://evaluation-technique.lundimatin.biz/api/');
+define('API_HEADERS', [
+    'Accept: application/api.rest-v1+json',
+    'Content-Type: application/json'
+]);
